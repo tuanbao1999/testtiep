e8a0e6145d70a8e6c84ff2f2d202198e35c2f575
tuanbao1999 <72750665+tuanbao1999@users.noreply.github.com>
2026-01-18 18:52:55 +0700

    Update Schedule.java

--- src/main/java/com/example/demo/application/schedule/Schedule.java
+++ src/main/java/com/example/demo/application/schedule/Schedule.java
@@ -15,20 +15,20 @@ public class Schedule {
     private static final String LOGIN_API_URL = "https://testtiep.onrender.com/api/admin/gen-api";
     private final RestTemplate restTemplate;
 
-    // Chạy mỗi 30 giây
-//    @Scheduled(cron = "0 * * * * ?")
-//    public void scheduleFixedRateTask() {
-//        System.out.println("đã vào");
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//        HttpEntity<String> request = new HttpEntity<>(headers);
-//        ResponseEntity<Object> response = restTemplate.exchange(
-//                LOGIN_API_URL,
-//                HttpMethod.GET,
-//                request,
-//                Object.class
-//        );
-//        System.out.println(response.getBody());
-//
-//    }
+    Chạy mỗi 30 giây
+   @Scheduled(cron = "0 * * * * ?")
+   public void scheduleFixedRateTask() {
+       System.out.println("đã vào");
+       HttpHeaders headers = new HttpHeaders();
+       headers.setContentType(MediaType.APPLICATION_JSON);
+       HttpEntity<String> request = new HttpEntity<>(headers);
+       ResponseEntity<Object> response = restTemplate.exchange(
+               LOGIN_API_URL,
+               HttpMethod.GET,
+               request,
+               Object.class
+       );
+       System.out.println(response.getBody());
+
+   }
 }
